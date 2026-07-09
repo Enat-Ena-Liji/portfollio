446a4a798a05ca2f8ea72a1b7a105a4f4ec0cda8
Daniel-Ayen <dayen0938@gmail.com>
2026-07-09 15:12:22 -0300

    update light mode to make attractive

--- my-porfolio/src/main.jsx
+++ my-porfolio/src/main.jsx
@@ -3,9 +3,18 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/global.css';
 
+// Set light theme by default
+const setTheme = () => {
+  const root = document.documentElement;
+  root.setAttribute('data-theme', 'light');
+};
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+// Set theme on initial load
+setTheme();
